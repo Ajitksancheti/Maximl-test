2ccc1c3d6ddb27ed55cce71da73d40613fef2e88
Ajitksancheti <43040198+Ajitksancheti@users.noreply.github.com>
2020-03-04 22:54:08 +0530

    Create question.py

---
+++ question.py
@@ -0,0 +1,13 @@
+inp=input()
+mini=100000
+k=len(set(inp))
+j=0
+i=0
+while i<=len(inp):
+    if len(set(inp[j:i+1]))==k:
+        mini=min(i+1-j,mini)
+        j+=1
+    else:
+        i+=1
+    i-=1    
+print(mini)
